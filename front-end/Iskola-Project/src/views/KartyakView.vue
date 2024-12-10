2190a04e152d0121f5b26a285022ea89745ab191
feherdorian <dorianfeherzsolt@gmail.com>
2024-12-10 12:21:20 +0100

    .

---
+++ front-end/Iskola-Project/src/views/KartyakView.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>Kártyák</h2>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return {
+      urlApi: "http://localhost:8000/api/",
+      rows:[],
+    };
+  },
+  mounted(){
+  this.getOsztalynevsor()
+  },
+  methods: {
+    async getOsztalynevsor() {
+      const url= '${this.url}/queryOsztalynevsor'
+      const request = await fetch(url);
+      this.rows = await request.json();
+      console.log(this.rows);
+      
+    }
+  },
+};
+</script>
